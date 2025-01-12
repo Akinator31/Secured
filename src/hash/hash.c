1916e96fbec7cd7010375893db3671ce3214a04b
Gaël Laguna <gael.laguna@epitech.eu>
2025-01-12 12:11:58 +0100

    feat: added functional hash function

--- src/hash/hash.c
+++ src/hash/hash.c
@@ -7,11 +7,21 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "hashtable.h"
 #include "my_stdlib.h"
 #include "my_string.h"
+#include "my_sha256.h"
 
+// The actual hash function is my_hash in lib/my_libs/my_sha256/
+// Using the sha256 secure hash function, we retrieve 256 bits of data,
+// However we only want an int (32 bits), so we only return 1/8 of the data.
 int hash(char *key, int len)
 {
-    return 0;
+    uint32_t digested_data = 0;
+    char *digested_message = my_hash(key);
+
+    my_memrcpy(&digested_data, &digested_message[(len % 8) << 2], 4);
+    free(digested_message);
+    return (int)digested_data;
 }
