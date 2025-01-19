a89e5c01121904d94c44bdf6d1b0b7cf4d734c0e
Pavel <pavel.de-wavrechin@epitech.eu>
2025-01-19 01:18:44 +0100

    Refactor: Fix loop that iterate too far in delate_hashtable function

--- src/hash_table/delete_hashtable.c
+++ src/hash_table/delete_hashtable.c
@@ -12,7 +12,7 @@ void delete_hashtable(hashtable_t *ht)
 {
     if (ht == MY_NULL)
         return;
-    for (int i = 0; i <= ht->size; ++i)
+    for (int i = 0; i < ht->size; ++i)
         ht->hashtable[i] = clear_list_and_data(ht->hashtable[i], &my_free);
     my_free(ht->hashtable);
     my_free(ht);
